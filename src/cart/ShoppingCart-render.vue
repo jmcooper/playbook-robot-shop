b593c7372b873e5516698ecdbd9b8a2644ab960b
Jim Cooper <jim@cooperplanet.com>
2023-11-13 17:25:06 -0700

    m3-c10: Fallthrough Attributes

---
+++ src/cart/ShoppingCart-render.vue
@@ -0,0 +1,67 @@
+<!-- <template>
+  <div class="container">
+    <h1>Your Cart</h1>
+    <EmptyCart v-if="cart.length === 0" />
+    <ul class="cart" v-if="cart.length > 0">
+      <li class="cart-item" v-for="(product, index) in cart" :key="index">
+        <ProductInfo :product="product">
+          <BaseButton v-on:click="removeFromCart(product)">Remove</BaseButton>
+        </ProductInfo>
+      </li>
+    </ul>
+    <div v-if="cart.length > 0" class="total">Total: {{ toCurrency(cartTotal) }}</div>
+  </div>
+</template> -->
+<script>
+import { h, resolveComponent } from 'vue'
+import { storeToRefs } from 'pinia'
+import EmptyCart from './EmptyCart.vue'
+import ProductInfo from '@/catalog/product-info/ProductInfo.vue'
+import { useCartStore } from '@/stores/cart'
+import { toCurrency } from '@/shared/formatters'
+
+export default {
+  render() {
+    const cartStore = useCartStore()
+    const { cart, cartTotal } = storeToRefs(cartStore)
+    const { removeFromCart } = cartStore
+    const BaseButton = resolveComponent('BaseButton')
+
+    const emptyCart = h(EmptyCart)
+    const cartItems = h('ul', { class: 'cart' },
+      cart.value.map((product, index) => h('li', { class: 'cart-item', key: index }, [
+        h(ProductInfo, { product }, () => h(BaseButton, { onClick: () => removeFromCart(product) }, () => 'Remove')),
+      ])),
+    )
+
+    return h('div', { class: 'container' }, [
+      h('h1', ['Your Cart']),
+      cart.value.length > 0 ? cartItems : emptyCart,
+      cart.value.length > 0 ? h('div', { class: 'total' }, [`Total: ${toCurrency(cartTotal.value)}`]) : null,
+    ])
+  },
+}
+</script>
+
+<style scoped>
+.cart {
+  margin: 0 100px;
+  border-top: 2px solid #999;
+}
+
+.cart-item {
+  border-bottom: 2px solid #999;
+}
+
+.total {
+  margin: 25px 175px 0 0;
+  align-self: right;
+  font-size: 25px;
+  text-align: right;
+}
+
+.empty-cart {
+  margin-top: 20px;
+  font-size: 20px;
+}
+</style>
--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -1,46 +1,29 @@
-<!-- <template>
+<template>
   <div class="container">
     <h1>Your Cart</h1>
     <EmptyCart v-if="cart.length === 0" />
     <ul class="cart" v-if="cart.length > 0">
       <li class="cart-item" v-for="(product, index) in cart" :key="index">
         <ProductInfo :product="product">
-          <BaseButton v-on:click="removeFromCart(product)">Remove</BaseButton>
+          <BaseButton @click="removeFromCart(product)">Remove</BaseButton>
         </ProductInfo>
       </li>
     </ul>
     <div v-if="cart.length > 0" class="total">Total: {{ toCurrency(cartTotal) }}</div>
   </div>
-</template> -->
-<script>
-import { h, resolveComponent } from 'vue'
+</template>
+
+<script setup>
 import { storeToRefs } from 'pinia'
 import EmptyCart from './EmptyCart.vue'
 import ProductInfo from '@/catalog/product-info/ProductInfo.vue'
 import { useCartStore } from '@/stores/cart'
 import { toCurrency } from '@/shared/formatters'
 
-export default {
-  render() {
-    const cartStore = useCartStore()
-    const { cart, cartTotal } = storeToRefs(cartStore)
-    const { removeFromCart } = cartStore
-    const BaseButton = resolveComponent('BaseButton')
+const cartStore = useCartStore()
+const { cart, cartTotal } = storeToRefs(cartStore)
+const { removeFromCart } = cartStore
 
-    const emptyCart = h(EmptyCart)
-    const cartItems = h('ul', { class: 'cart' },
-      cart.value.map((product, index) => h('li', { class: 'cart-item', key: index }, [
-        h(ProductInfo, { product }, () => h(BaseButton, { onClick: () => removeFromCart(product) }, () => 'Remove')),
-      ])),
-    )
-
-    return h('div', { class: 'container' }, [
-      h('h1', ['Your Cart']),
-      cart.value.length > 0 ? cartItems : emptyCart,
-      cart.value.length > 0 ? h('div', { class: 'total' }, [`Total: ${toCurrency(cartTotal.value)}`]) : null,
-    ])
-  },
-}
 </script>
 
 <style scoped>
