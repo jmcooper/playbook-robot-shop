269fce41165242ce0a8fc1dba967e5f7ee08484b
Jim Cooper <jim@cooperplanet.com>
2023-11-16 12:49:03 -0700

    m5-c3: readonly()

--- src/stores/product.js
+++ src/stores/product.js
@@ -1,5 +1,5 @@
 import { defineStore, acceptHMRUpdate } from 'pinia'
-import { ref } from 'vue'
+import { ref, readonly } from 'vue'
 
 export const useProductStore = defineStore('products', () => {
   const products = ref([])
@@ -9,14 +9,14 @@ export const useProductStore = defineStore('products', () => {
     const response = await fetch('/api/products')
     const returnedProducts = await response.json()
 
-    products.value = returnedProducts
+    products.value = readonly(returnedProducts)
 
     return returnedProducts
   }
 
   async function getInventory(productId) {
     const response = await fetch(`/api/inventory/${productId}`)
-    
+
     const inventory = await response.json()
     return inventory.inventoryCount
   }
